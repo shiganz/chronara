e703d8b3f8479d0a95b7a3a982b9675a0e34aae5
Dens <153103221+shiganz@users.noreply.github.com>
2026-03-31 15:19:49 +0800

    feat: update minikit.config.ts with account association

--- minikit.config.ts
+++ minikit.config.ts
@@ -1,31 +1,30 @@
-const ROOT_URL =
-  process.env.NODE_ENV === "production"
-    ? `${process.env.NEXT_PUBLIC_ROOT_URL}`
-    : "http://localhost:3000";
+const ROOT_URL = "https://chronara-three.vercel.app";
 
 export const minikitConfig = {
   accountAssociation: {
-    header: "",
-    payload: "",
-    signature: "",
+    header: "eyJmaWQiOjIzNzk2NzMsInR5cGUiOiJhdXRoIiwia2V5IjoiMHhCM0M5OEU5RWY0M2Y3ODJEZWIwOTVjMGM1NTcyRjZCNjQ4QzZENDExIn0",
+    payload: "eyJkb21haW4iOiJjaHJvbmFyYS10aHJlZS52ZXJjZWwuYXBwIn0",
+    signature: "AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAACAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAABAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAEErPfCBZ0GRNIfJBDVNyvGJUjVKnu1Ruj0Wg0pJ-hm_0EushR3MHPMy5r8DJOzjh-7_oDIPjUuZB_RZ0lfUTda6GwAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA",
   },
   miniapp: {
     version: "1",
-    name: "Miniapp Name",
-    subtitle: "Miniapp subtitle",
-    description: "Miniapp Description",
-    screenshotUrls: [`${ROOT_URL}/image.png`],
-    iconUrl: `${ROOT_URL}/image.png`,
-    splashImageUrl: `${ROOT_URL}/image.png`,
-    splashBackgroundColor: "#000000",
+    name: "Chonara",
+    subtitle: "Escape through collapsing timelines",
+    description: "Escape through collapsing timelines",
+    screenshotUrls: ["https://chronara-three.vercel.app/image.png"],
+    iconUrl: "https://chronara-three.vercel.app/image.png",
+    splashImageUrl: "https://chronara-three.vercel.app/image.png",
+    splashBackgroundColor: "#e78636",
     homeUrl: ROOT_URL,
-    webhookUrl: `${ROOT_URL}/api/webhook`,
-    primaryCategory: "social",
-    tags: ["marketing", "ads", "quickstart"],
-    heroImageUrl: `${ROOT_URL}/image.png`,
-    tagline: "your tagline here",
-    ogTitle: "Miniapp Name",
-    ogDescription: "Miniapp Description",
-    ogImageUrl: `${ROOT_URL}/image.png`,
+    webhookUrl: "https://chronara-three.vercel.app/api/webhook",
+    primaryCategory: "games",
+    tags: ["adventure", "game", "miniapp", ""],
+    heroImageUrl: "https://chronara-three.vercel.app/image.png",
+    tagline: "Play Endlessly!",
+    ogTitle: "Chonara",
+    ogDescription: "Escape through collapsing timelines",
+    ogImageUrl: "https://chronara-three.vercel.app/image.png",
+    "noindex": true
+
   },
 } as const;
